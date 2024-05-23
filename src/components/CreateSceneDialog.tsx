d34b511f0dc08011e187ab245e0096647730ad16
Ian <ian.farre@mia-platform.eu>
2024-05-23 23:10:33 +0200

    fix: allow only certain characters as scene name

--- src/components/CreateSceneDialog.tsx
+++ src/components/CreateSceneDialog.tsx
@@ -1,7 +1,8 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@mui/material';
 import { invoke } from '@tauri-apps/api';
 import { message } from '@tauri-apps/api/dialog';
-import React, { useState } from 'react';
+import type { ChangeEvent, FormEvent } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import type { Scene } from '../types/scene';
 
@@ -13,22 +14,37 @@ type CreateSceneDialogProps = {
 }
 
 export default function CreateSceneDialog(props: CreateSceneDialogProps) {
-  const [isSceneNameTaken, setIsSceneNameTaken] = useState(false);
+  const [validationText, setValidationText] = useState<string | undefined>();
+
+  const onChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    const sceneName = event.target.value;
+
+    const isSceneNameTaken = props.scenes.some(scene => scene.name === sceneName);
+    if (isSceneNameTaken) {
+      setValidationText('A scene with that name already exists');
+      return;
+    }
+
+    const isSceneNameValid = /^([a-z0-9-_]*)$/.test(sceneName);
+    if (!isSceneNameValid) {
+      setValidationText('A scene name can only contain alfanumeric lowercase characters, - and _');
+      return;
+    }
+
+    setValidationText(undefined);
+  }, [props.scenes]);
 
   return (
     <Dialog
       PaperProps={{
         component: 'form',
-        onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+        onSubmit: (event: FormEvent<HTMLFormElement>) => {
           event.preventDefault();
           const formData = new FormData(event.currentTarget);
           const formJson = Object.fromEntries(formData.entries());
           const { sceneName } = formJson;
 
-          const isSceneNameTaken = props.scenes.some(scene => scene.name === sceneName);
-          setIsSceneNameTaken(isSceneNameTaken);
-
-          if (!isSceneNameTaken) {
+          if (validationText === undefined) {
             invoke('create_scene', { sceneName })
               .then(() => props.onAfterCreateScene())
               .catch(error => message(error as string, { title: 'Error', type: 'error' }));
@@ -46,11 +62,12 @@ export default function CreateSceneDialog(props: CreateSceneDialogProps) {
         </DialogContentText>
         <TextField
           autoFocus
-          error={isSceneNameTaken}
+          error={validationText !== undefined}
           fullWidth
-          helperText={`${isSceneNameTaken ? 'A scene with that name already exists' : ''}`}
+          helperText={validationText}
           label='Scene name'
           name='sceneName'
+          onChange={onChange}
           required
           size='small'
           type='text'
